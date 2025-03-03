d7b41bfc8eb6a5f6e704cc557c3bb46990341466
rypetrat <rypetrat@bu.edu>
2025-03-03 12:36:08 -0500

    copied over each page but projects cause gotta to the js part :(

--- pages/Education.tsx
+++ pages/Education.tsx
@@ -1,7 +1,36 @@
 export default function Education() {
     return (
-      <div>
-        <h1>Education Page</h1>
-      </div>
+      <>
+        <h2 id="education-title">Education</h2>
+        <div id="education">
+          <div id="education-text">
+            <img src="../src/assets/bu.png" id="education-image" alt="Boston University Crest"/>
+            <p>
+                I began my college tenure at Boston University in the fall of 2021 and I will be graduating from BU with a BA in Computer Science 
+                at the conclusion of the spring 2025 Semester. Go Terriers!
+            </p>
+            
+            <h3>Courses Taken:</h3>
+
+            <ul>
+                <li><b>CS460</b>, Introduction to Databases Systems</li>
+                <li><b>CS455</b>, Computer Networks</li>
+                <li><b>CS440</b>, Introduction to Artificial Intelligence</li>
+                <li><b>CS412</b>, Full Stack Engineering</li>
+                <li><b>CS411</b>, Software Engineering</li>
+                <li><b>CS391</b>, Web Application Development</li>
+                <li><b>CS350</b>, Fundamentals of Computing Systems</li>
+                <li><b>CS330</b>, Introduction to Algorithm Design</li>
+                <li><b>CS320</b>, Concepts of Programming Languages</li>
+                <li><b>CS237</b>, Probability and Statistics in Computing</li>
+                <li><b>CS210</b>, Computer Systems</li>
+                <li><b>CS132</b>, Linear Algebra</li>
+                <li><b>CS131</b>, Discrete Math</li>
+                <li><b>CS112</b>, Introduction to Computer Science 2</li>
+                <li><b>CS111</b>, Introduction to Computer Science 1</li>
+            </ul>
+          </div>
+        </div>
+      </>
     );
   }
--- pages/References.tsx
+++ pages/References.tsx
@@ -1,7 +1,18 @@
 export default function References() {
     return (
-      <div>
-        <h1>References Page</h1>
-      </div>
+      <>
+        <h2 id="references-title">References</h2>
+          <div id="references">
+              <ul>
+                  <li><strong>Anandh Ramchandran</strong> <b>(Manager)</b>, <b>Email</b>: AnandhR@gmail.com, <b>Phone</b>: 321-654-0987, Was my mentor in my time working for GEICO, provided invaluable assistance in not only implementing concise and efficient technical solutions to buisness problems but also provided great explanations of the context of these problems.</li>
+                  <li><strong>Ashish Singh</strong> <b>(Manager)</b>, <b>Email</b>: AshishSingh@gmail.com, <b>Phone</b>: 456-123-7890, Was my team's manager and was responsible with ensuring I was both doing work I found interesting as well as providing great insight into my work performance from a managerial prospective.</li>
+                  <li><strong>Ashish Hunnargikar</strong> <b>(Manager)</b>, <b>Email</b>: AshishHun@gmail.com, <b>Phone</b>: 123-456-7890, Was a parter team's manager that worked very closely with the team that I was working on that provided an external view of how our team's work impacted the broader tech stack.</li>
+                  <li><strong>Jonathan Merino</strong> <b>(Coworker)</b>, <b>Email</b>: JonathanMerino@gmail.com, <b>Phone</b>: 123-456-0987, Was a great coworker who made working in a corporate space feel more comfortable by being very down to earth and explaining what our team's objective is in terms that were very easy to understand which made my work not only easier to understand and contextualize as well as helping optimize my various code implementations.</li>
+                  <li><strong>Azamat Kalilov</strong> <b>(Coworker)</b>, <b>Email</b>: AzamatKalilov@gmail.com, <b>Phone</b>: 321-456-7890, Was a coworker on the team who mainly worked on implementing task automation which was not something I was previously very experiences with but provided me with significant context into why these automations are needed as well as the frameworks and how they should be implemented in a effective and maintainable way.</li>
+                  <li><strong>Jerry Wang</strong> <b>(Co-intern)</b>, <b>Email</b>: JerryWang@gmail.com, <b>Phone</b>: 123-654-7890, Another intern in the summer internship program I was in who helped both motivate and assist me throughout my time working for GEICO as an intern.</li>
+                  <li><strong>Vishesh Prasad</strong> <b>(Co-intern)</b>, <b>Email</b>: VisheshPrasad@gmail.com, <b>Phone</b>: 321-654-7890, Another intern in the summer internship program I was in who helped both motivate and assist me throughout my time working for GEICO as an intern.</li>
+              </ul>
+          </div>
+      </>
     );
   }
